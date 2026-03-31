335cd781468aed8cfbabaa3df4d091564471dbd4
Moises <gerardomoises544@gmail.com>
2026-03-31 01:47:25 -0500

    areglo error

--- src/main/java/edu/pe/cibertec/infracciones/InfraccionesApplication.java
+++ src/main/java/edu/pe/cibertec/infracciones/InfraccionesApplication.java
@@ -4,6 +4,7 @@ import edu.pe.cibertec.infracciones.config.DotenvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class InfraccionesApplication {
 
